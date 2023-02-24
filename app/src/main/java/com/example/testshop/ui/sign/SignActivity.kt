ee7b8f279da19fbe2e8ccae2c45b1c0d391d7115
Samim <Samim058@gmail.com>
2023-02-24 18:30:57 +0300

    Refactor UI. Add dependency.

--- app/src/main/java/com/example/testshop/ui/sign/SignActivity.kt
+++ app/src/main/java/com/example/testshop/ui/sign/SignActivity.kt
@@ -2,6 +2,8 @@ package com.example.testshop.ui.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import com.example.testshop.R
 import com.example.testshop.databinding.ActivitySignBinding
@@ -21,6 +23,27 @@ class SignActivity : AppCompatActivity() {
             val intent = LoginActivity.newIntent(this)
             startActivity(intent)
         }
+        isEmailValid()
+    }
 
+    private fun isEmailValid() {
+        binding.editTextEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val email = binding.editTextEmail.text?.trim().toString()
+                val valid = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+                if (!valid) {
+                    binding.tilEmail.error = INVALID_ADDRESS
+                } else {
+                    binding.tilEmail.error = EMPTY_FIELD
+                }
+            }
+        })
+    }
+
+    companion object{
+        private const val EMPTY_FIELD = ""
+        private const val INVALID_ADDRESS = "Invalid Email address"
     }
 }
